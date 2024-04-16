7712bbdbdb2ce0d6504c375c6e9253f5004bc632
Aakarsh Pathak <113720231+Aakarsh204@users.noreply.github.com>
2024-04-16 22:42:27 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I use it to code and game on. I built all my projects on it, including the portfolio you're traversing right now.
+     I mostly work with Javascript and Java (Guess you could say I like coffee). Regardless, I generally like programming.
+     Here is my <a href="https://github.com/Aakarsh204" target="_blank">Github</a> and my <a href="https://www.linkedin.com/in/aakarsh-pathak-3b5550203/" target="_blank">LinkedIn</a>.`,
+    "cs-degree": `This is my upcoming CS degree. I'm still a sophomore so it doesn't exist yet, but it soon will.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+     I also game on my PS2 sometimes.`,
+    tv: `That's my TV. I've been watching movies since I was a kid and I'm a huge cinephile. You can check out my <a href="https://letterboxd.com/Aakarsh204/" target="_blank">LetterBoxd</a> here.`,
+    bed: `This where I sleep. Or atleast try to. I'm trying to figure out a polyphasic cycle for myself. You can try it too at <a href="https://www.polyphasic.net/" target="_blank">polyphasic.net</a>`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/Aakarsh204/Apakt-204/blob/f5be327f4888d807379e35ff533d194f7b3b064a/Resume.pdf" target="_blank">Check it out?</a>
+     Contact me at aakarsh204@gmail.com if you have any interesting internship opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. I also used a publically sourced sprite pack.`,
+    library: `There are a lot of books on my shelves. I love reading spy thrillers like Bourne, Reacher and Alex Rider.
+     Alex Rider is probably the best series I've ever read. Thank you Horowitz for that masterpiece. There are also a lot of books on magic.
+     I probably only read one of them. Who else compulsively downloads books without ever finishing them?`,
+    exit: `If you want to exit Aakarsh's portfolio, just close the tab.`,
+};
